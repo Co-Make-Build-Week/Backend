68484d5b7b099daeb3072d5eebecae6c623e4520
Eleasah Halsmer <eleasahjh@gmail.com>
2019-09-23 11:38:44 -0600

    pre pull commit

---
+++ issues/issues-router.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const Users = require('../users/users-model.js')
+const secrets = require('../config/secrets.js')
+
+router.post('/', (req, res) => {
+
+})
+
+router.post('/', (req, res) => {
+
+})
+
+router.get('/', (req, res) => {
+
+})
+
+
+
+module.exports = router;
