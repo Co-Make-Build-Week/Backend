d2a28251cbe76ba75fc20ab6386beb8f6426efa4
Eleasah Halsmer <eleasahjh@gmail.com>
2019-09-23 11:02:07 -0600

    commit before pull

---
+++ issues/issues-model.js
@@ -0,0 +1,28 @@
+const db = require('../database/db-config.js')
+
+module.exports = {
+  find,
+  add,
+  findBy,
+  findById
+}
+
+//may not be necessary
+function find() {
+  return db('issues')
+}
+
+async function add(user) {
+  const [id] = await db('issues').insert(issue)
+  return findById(id)
+}
+
+function findBy(filter) {
+  return db('issues').where(filter)
+}
+
+function findById(id) {
+  return db('issues')
+  .where({ id })
+  .first();
+}
