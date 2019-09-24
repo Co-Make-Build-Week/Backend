98eab001a25b0fe7c68cfb541420d51ab0d2ed16
Eleasah Halsmer <eleasahjh@gmail.com>
2019-09-23 18:34:47 -0600

    Fixed conditional for uniqueUsername other-middleware

--- api/other-middleware.js
+++ api/other-middleware.js
@@ -13,7 +13,8 @@ function uniqueUsername(req, res, next){
     const username = req.body.username;
     Users.findBy({username})
     .then(response => {
-        if (response) {
+        console.log(response);
+        if (response.length > 0) {
             res.status(400).json({message: 'A user with that email already exists'})
         } else {
             next();
