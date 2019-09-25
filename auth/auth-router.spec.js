bbd0dc1ec984b6386da1244f1bd061d377516f96
Bryan Szendel <bryan.szendel@gmail.com>
2019-09-25 17:19:37 -0600

    helper model tests for auth-router

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -5,11 +5,28 @@ const Users = require('../users/users-model.js');
 const server = require('../api/server.js');
 
 describe('auth-router.js', () => {
-  describe('GET /api/auth/users', () => {
-    it('returns 200 OK', () => {
-      return request(server).get('/api/auth/users').then(res => {
-        expect(res.status).toBe(200)
-      })
+
+  beforeEach(async () => {
+    await db('users').truncate();
+  })
+
+  describe('add() helper model', () => {
+    // it('returns 201 OK', async () => {
+    //   // const expectedStatusCode = 201
+    //   const response = await request(server).post('/api/auth/register').send({username: 'testing', password: '1234'});
+    //   await expect(response.status).toBe(201)
+    // })
+    it('adds the users', async () => {
+      const user = await Users.add({username: 'john', password: '1234'})
+      // const {username} = await db('users')
+      expect(user.username).toBe('john')
+      expect(user.password).toBe('1234')
+    });
+    it('returns the users', async () => {
+      await Users.add({username: 'carlo', password: '1234'})
+      await Users.add({username: 'bryan', password: '1234'})
+      const response = await Users.find()
+      expect(response.length).toBe(2)
     })
   })
 })
