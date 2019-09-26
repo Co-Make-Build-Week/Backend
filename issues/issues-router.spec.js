c2ecc9068068c008891459c0405d865a0753ec69
Eleasah Halsmer <eleasahjh@gmail.com>
2019-09-26 15:35:51 -0600

    Added test for PUT /:id

--- issues/issues-router.spec.js
+++ issues/issues-router.spec.js
@@ -19,7 +19,16 @@ async function post() {
     .send({ title: "Test title", category: "roads" })
     .set("authorization", token);
   const issue = response.body;
-  return {issue, token}
+  return { issue, token };
+}
+
+async function put(id, newCategory, token) {
+  const response = await request(server)
+    .put(`/api/issues/${id}`)
+    .send({category: `${newCategory}`})
+    .set("authorization", token);
+  const updatedIssue = response;
+  return updatedIssue;
 }
 
 describe("Issues router", () => {
@@ -61,9 +70,9 @@ describe("Issues router", () => {
 
   describe("POST /api/issues", () => {
     beforeEach(async () => {
-        await db("users").truncate();
-        await db("issues").truncate();
-      });
+      await db("users").truncate();
+      await db("issues").truncate();
+    });
 
     it("returns 201 and issue with valid token and issue", async () => {
       const token = await registerToken();
@@ -83,6 +92,7 @@ describe("Issues router", () => {
         );
       expect(response.status).toBe(401);
     });
+    // todo: write middleware to validate new issues, return 400 if missing required fields
     it("returns 500 if category is not included on issue", async () => {
       const token = await registerToken();
       const response = await request(server)
@@ -91,29 +101,44 @@ describe("Issues router", () => {
         .set("authorization", token);
       expect(response.status).toBe(500);
     });
-    
   });
-  describe('/:id', () => {
-      beforeEach(async () => {
-        await db("users").truncate();
-        await db("issues").truncate();
-      });
 
-      it('Post issue, request same issue on /:id, receive issue and status 200', async () => {
-        const {issue, token} = await post();
-        const response = await request(server)
-          .get(`/api/issues/${issue.id}`)
-          .set("authorization", token);
-        expect(response.status).toBe(200);
-        expect(response.body.id).toBe(issue.id)
-        expect(response.body.category).toBe(issue.category);
-      });
-      it('Returns 404 not found  and message if issue does not exist', async () => {
-        const token = await registerToken();
-        const response = await request(server)
-        .get('/api/issues/3')
+  describe("GET /:id", () => {
+    beforeEach(async () => {
+      await db("users").truncate();
+      await db("issues").truncate();
+    });
+
+    it("POST issue, GET same issue on /:id, receive issue and status 200", async () => {
+      const { issue, token } = await post();
+      const response = await request(server)
+        .get(`/api/issues/${issue.id}`)
         .set("authorization", token);
-        expect(response.status).toBe(404);
-      });
+      expect(response.status).toBe(200);
+      expect(response.body.id).toBe(issue.id);
+      expect(response.body.category).toBe(issue.category);
+    });
+    it("GET returns 404 not found and message if issue does not exist", async () => {
+      const token = await registerToken();
+      const response = await request(server)
+        .get("/api/issues/3")
+        .set("authorization", token);
+      expect(response.status).toBe(404);
+    });
+  });
+
+  describe("PUT /:id", () => {
+    beforeEach(async () => {
+      await db("users").truncate();
+      await db("issues").truncate();
+    });
+
+    it('POST issue, PUT same issue with different category, receive 201 and updated category', async () => {
+     const { issue, token } = await post();
+     const updatedIssue = await put(issue.id, 'landscape', token)
+     expect(updatedIssue.status).toBe(201);
+     expect(updatedIssue.body.id).toBe(issue.id);
+     expect(updatedIssue.body.category).toBe('landscape');
+    });
   });
 });
