cd58c9883192b2825caa6c530548cbc3423c4cfd
Eleasah Halsmer <eleasahjh@gmail.com>
2019-09-26 12:04:33 -0600

    Tests on /api/issues

--- issues/issues-router.spec.js
+++ issues/issues-router.spec.js
@@ -1,10 +1,16 @@
 const request = require('supertest');
 const server = require('../api/server');
 
-describe('server', () => {
-    it('should return an OK status code from /api/issues route', async () => {
-        const get = await request(server).get('/api/auth/users')
-        expect(get.status).toBe(201)
-
+describe('/api/issues', () => {
+    it("should return 400 Bad Request and 'Please provide a token on authorization header' if no token is sent", async () => {
+        const get = await request(server).get('/api/issues')
+        expect(get.status).toBe(400)
+        expect(get.body.message).toBe('Please provide a token on authorization header')
     });
+    it("should return 401 Unauthorized and 'Invalid token' if bad token is sent", async () => {
+        const get = await request(server).get('/api/issues').set('authorization', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImVsZWFzYWhAbGFtYmRhLmNvbSIsInVzZXJpZCI6NSwiaWF0IjoxNTY5NTE5MjIyLCJleHAiOjE1Njk5NTEyMjJ9.SGZMwP_w7SLzwwVPXNeLm4am6qZ1sxOgx1RWtv0eLB0')
+        expect(get.status).toBe(401)
+        expect(get.body.message).toBe('Invalid token')
+    });
+
 });
