4e5951fcd2c5c80cc4df47d2a6be3b370a3df007
Eleasah Halsmer <eleasahjh@gmail.com>
2019-09-26 14:24:08 -0600

    Another /api/issues post test

--- issues/issues-router.spec.js
+++ issues/issues-router.spec.js
@@ -12,34 +12,50 @@ async function registerToken() {
   return token;
 }
 
-
-describe("/api/issues", () => {
-  beforeEach(async () => {
-    await db("users").truncate();
-    await db("issues").truncate();
-  });
-  it("should return 400 Bad Request and 'Please provide a token on authorization header' if no token is sent", async () => {
-    const get = await request(server).get("/api/issues");
-    expect(get.status).toBe(400);
-    expect(get.body.message).toBe(
-      "Please provide a token on authorization header"
-    );
-  });
-  it("should return 401 Unauthorized and 'Invalid token' if bad token is sent", async () => {
-    const get = await request(server)
-      .get("/api/issues")
-      .set(
-        "authorization",
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImVsZWFzYWhAbGFtYmRhLmNvbSIsInVzZXJpZCI6NSwiaWF0IjoxNTY5NTE5MjIyLCJleHAiOjE1Njk5NTEyMjJ9.SGZMwP_w7SLzwwVPXNeLm4am6qZ1sxOgx1RWtv0eLB0"
-      );
-    expect(get.status).toBe(401);
-    expect(get.body.message).toBe("Invalid token");
-  });
-  it("should return 200 and list of issues with a valid token", async () => {
-    const token = await registerToken();
-    // console.log("TOKEN", token)
-    const response = await request(server).get("/api/issues").set("authorization", token)
-    expect(response.status).toBe(200);
-    // console.log(response.body);
-  });
+describe('Issues router', () => {
+    beforeEach(async () => {
+        await db("users").truncate();
+        await db("issues").truncate();
+      });
+    describe("GET /api/issues", () => {
+        it("should return 400 Bad Request and 'Please provide a token on authorization header' if no token is sent", async () => {
+          const get = await request(server).get("/api/issues");
+          expect(get.status).toBe(400);
+          expect(get.body.message).toBe(
+            "Please provide a token on authorization header"
+          );
+        });
+        it("should return 401 Unauthorized and 'Invalid token' if bad token is sent", async () => {
+          const get = await request(server)
+            .get("/api/issues")
+            .set(
+              "authorization",
+              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImVsZWFzYWhAbGFtYmRhLmNvbSIsInVzZXJpZCI6NSwiaWF0IjoxNTY5NTE5MjIyLCJleHAiOjE1Njk5NTEyMjJ9.SGZMwP_w7SLzwwVPXNeLm4am6qZ1sxOgx1RWtv0eLB0"
+            );
+          expect(get.status).toBe(401);
+          expect(get.body.message).toBe("Invalid token");
+        });
+        it("should return 200 and list of issues with a valid token", async () => {
+          const token = await registerToken();
+          const response = await request(server).get("/api/issues").set("authorization", token)
+          expect(response.status).toBe(200);
+        });
+      });
+    describe('POST /api/issues', () => {
+        it('returns 201 and issue with valid token and issue', async () => {
+          const token = await registerToken();
+          const response = await request(server).post("/api/issues").send({title: "Test title", category: "roads"}).set("authorization", token)
+          expect(response.status).toBe(201);
+        });
+        it('returns 401 if invalid token is sent', async () => {
+          const response = await request(server).post("/api/issues").send({title: "Test title", category: "roads"}).set("authorization", 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImVsZWFzYWhAbGFtYmRhLmNvbSIsInVzZXJpZCI6NSwiaWF0IjoxNTY5NTE5MjIyLCJleHAiOjE1Njk5NTEyMjJ9.SGZMwP_w7SLzwwVPXNeLm4am6qZ1sxOgx1RWtv0eLB0')
+          expect(response.status).toBe(401);
+        });
+        it('returns 500 if category is not included on issue', async () => {
+          const token = await registerToken();
+          const response = await request(server).post("/api/issues").send({title: "Test title"}).set("authorization", token)
+          expect(response.status).toBe(500);
+        })
+    });
 });
+
